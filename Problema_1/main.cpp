2a1c391afc8e6e3cbfd556a19df509ac29f8455a
joseduque1 <jose.duque1@udea.edu.co>
2022-07-05 15:07:58 -0500

    Fin del problema 1

--- Problema_1/main.cpp
+++ Problema_1/main.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 int main()
 {
-    cout << "Inicio del problema 1" << endl;
-    return 0;
+    int cin_mil = 50000;
+    int v_mil = 20000;
+    int mil = 2000;
+    int dos = 200;
+    int resto = 0;
+    int modulo = 0;
+    int num = 0;
+    cout << "Ingresa un numero entero: ";
+    cin >> num;
+
+    resto = num / cin_mil;
+    num = num % cin_mil;
+
+    cout << cin_mil << " : "<<resto<<endl;
+
+    while(v_mil >= 5000){
+        resto = num / v_mil;
+        cout << v_mil << " : "<<resto<<endl;
+        num = num%v_mil;
+        v_mil = v_mil/2;
+    }
+
+    while(mil >= 500){
+        resto = num / mil;
+        cout << mil << " : "<<resto<<endl;
+        num = num%mil;
+        mil = mil/2;
+    }
+    while(dos >= 50){
+        resto = num / dos;
+        cout << dos << " : "<<resto<<endl;
+        num = num%dos;
+        dos = dos/2;
+    }
+    cout<< "faltante : "<<num<<endl;
 }
